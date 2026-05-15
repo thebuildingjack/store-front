6d17ab16de66969b007fff23153f671314d0a228
Francis Authority <francis.i.authority@gmail.com>
2026-05-15 10:05:39 -0700

    fix prisma client generation

--- lib/Prisma.ts
+++ lib/Prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+/* import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
@@ -9,8 +9,30 @@ export const prisma =
   globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
+ */
 
 /* DATABASE_URL="postgresql://postgres.tykowgyngmcotmrxirdz:DBAuthProject@2002@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
 
 JWT_SECRET="my-super-secret-key-for-my-store-front" */
+
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
