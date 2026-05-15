354512620782aa013269627f183566e04cdf51b0
Francis Authority <francis.i.authority@gmail.com>
2026-05-15 21:48:01 +0100

    login successful.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      <ThemeSwitcher />
-      <h1 className="font-bold font-heading text-accent text-3xl">STORE FRONT</h1>
+    <div className="relative p-10 flex flex-col flex-1 items-center justify-start">
+      <div className="absolute top-10 right-10">
+        <ThemeSwitcher />
+      </div>
+      <h1 className="mt-64 font-bold font-heading text-accent text-3xl">WELCOME TO THE STORE FRONT</h1>
     </div>
   );
 }
